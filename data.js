aecf4743b39444979ce35fe63555fad5d44e1657
Nguyen Gia Bach <teppeitaiki@gmail.com>
2021-04-06 16:59:35 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,144 @@
+let foodData = [{
+    title: `COOK`,
+    id: `1`,
+    listStyle: [{
+        id: `1`,
+        title: `Ẩm thực Việt Nam`,
+        listTitle: [{
+            title: `Món ngon hàng ngày`,
+            id: `1`,
+            menu: [{
+                id: `1`,
+                name: `Gà nướng ngũ vị`,
+                describe: `Đây là món ăn truyền thống dinh dưỡng cao vitaminc nhiều...`,
+                ingredient: [`1,5kg gà ta`, `50g ớt sừng không cay`, `20g ớt xa`, `10g ớt khô Huế`, `20g hành tím khô`, `20 tỏi`, `20g tương ớt`, `10ml màu điều 10g bột thịt gà`, `súp nền lẩu Thái 10ml dầu ăn`],
+                making: [
+                    `Bước 1: Lấy hành tím bóc sạch vỏ rửa sạch và tỏi cũng làm tương tự như thế để qua một bên trong khay nhỏ để sau đó chế cho tiện lợi.`,
+                    `Bước 2: Tổng hợp gia vị đã chuẩn bị sẵn ở trên vào máy sinh tố và cứ thế xay thật nhuyễn. Sau đó cho nước xốt ra một to lớn hoặc có thể để nguyên trong máy xay rồi tắt máy`,
+                    `Bước 3: lấy gà đã được làm sạch rồi đem ướp với nước xốt đã vừa mới chế biến khi nãy vào bên trong cũng như bên ngoài da của gà. Và chúng ta nên để khoảng 20 phút cho nước xốt thấm đều. Sau đó tiếp tục cho vào lò nướng ở nhiệt độ 180 nướng khoảng 25 phút. Sau khi gà chín vàng thị lấy ra`    
+                ],
+                price: `50.000 vnd`,
+                price_number: 50000,
+                numberPeople: `4`,
+                prepare: `30 minutes`,
+                cook: `6 hours`
+            }, {
+                id: `2`,
+                name: `Thịt lợn xào tỏi`,
+                describe: `Món ăn giàu sắt tốt cho da`,
+                ingredient: [`500g Thịt lợn ba chỉ`, `Gừng, xì dầu`, `dầu lạc`, `hành lá`, `gia vị`, `tỏi tây`],
+                making: [
+                    `Bước 1: Sơ chế thật sạch tỏi tây, cắt vát từng khúc vừa ăn`,
+                    `Bước 2: Thái thịt thành từng miếng mỏng hoặc khúc dài vừa miếng.Nên lựa chọn thịt ba chỉ để khi xào không bị khô.`,
+                    `Bước 3: Bắc chảo lên bếp cho dầu lạc vào, thêm hành lá, gừng và thịt heo vào đảo chín đều.`
+                    `Bước 4: Đổ tỏi tây đã sơ chế vào chảo đảo nhanh tay trên ngọn lửa lớn. Hành tây vừa chín tới cho ra đĩa ăn nóng cùng cơm nóng hoặc làm món nhậu đều rất ngon.`
+                ]
+                price: `40.000 vnd`,
+                price_number: 40000,
+                numberPeople: `4`,
+                prepare: `10 minutes`,
+                cook: `30 minutes`
+            }]
+            title: `Combo gia đình`
+            id: `2`,
+            menu: [{
+                id: `1`,
+                name: `COMBO 1:Mực nhồi thịt sốt cà chua - Rau lang luộc chấm mắm - Cà pháo - Canh mướp nấu rau dền`,
+                describe: ``,
+                ingredient:[{
+                    name: `Mực nhồi thịt sốt cà chua`,
+                    ingredient: [`mực 6 con`, `thịt nạc xay 150g`, `thịt mộc 100g`, `nấm mèo băm nhỏ 30g`, `hành tím băm 10g`, `đường 1 muỗng cà phê`, `muối ½ muỗng cà phê`, `bột ngọt ½ muỗng cà phê`, `tiêu ½ muỗng cà phê`, `nước mắm 2 muỗng cà phê`],
+                    making: [
+                        `Bước 1: Mực ống mua về các bạn sơ chế thật sạch, phần râu mực dài bạn cắt ra thành miếng nhỏ. Để ráo.`,
+                        `Bước 2: Cho toàn bộ phần thịt băm, mộc, nấm mèo, hành tím băm vào 1 tô lớn. Nêm tiếp vào tô 1 muỗng cà phê đường, 1/2 muỗng cà phê muối, 1/2 muỗng cà phê bột ngọt, 1/2 muỗng cà phê tiêu, 2 muỗng cà phê nước mắm.Trộn đều tay để các nguyên liệu hòa quyện vào nhau. Ướp phần nhân khoảng 15 phút cho thấm vị.Trộn đều tay để các nguyên liệu hòa quyện vào nhau. Ướp phần nhân khoảng 15 phút cho thấm vị.`,
+                        `Bước 3: Dùng tay nhồi nhân vào trong thân mực, gắn phần đầu vào và cố định bằng que tăm.Phần nhân dư bạn có thể vo viên và chiên cùng với mực nhé!`,
+                        `Bước 4: Bắc chảo lên bếp, cho dầu ăn vào. Dầu nóng các bạn cho mực vào chiên đến khí chín vàng thì vớt ra. Để ráo dầu.Bắc 1 chiếc chảo khác lên bếp, cho dầu ăn vào. Dầu nóng, bạn cho vào nồi 4 muỗng canh nước mắm, 6 thìa cà phê đường, 1/2 muỗng cà phê ớt bột, đảo đều. Trút toàn bộ phần mực vừa chiên vào chảo, rim thêm 3 phút để mực thấm sốt.Cho mực ra dĩa, trang thí thêm rau sống ăn kèm cho đẹp mắt là hoàn thành.`
+                    ],
+                },
+                {
+                    name: `Rau lang luộc chấm mắm gừng`,
+                    ingredient: [`1 bó rau khoang lang`, `1 nhánh gừng, ớt và đường`],
+                    making: [
+                        `Bước 1: Rau khoai lang ngắt bỏ phần già, rửa sạch để cho ráo nước`,
+                        `Bước 2: Đun nước sôi, cho rau vào luộc khoảng 2 phút thì tắt bếp và vớt rau ra`,
+                        `Bước 3: Pha nước mắm gừng: Bỏ gừng, ớt và đường vào cối hoặc bát nhỏ, Dùng chày giã cho nhuyễn. Sau đó cho thêm 1 thìa nước mắm và khuấy đều`,
+                        `Bước 4: Chấm rau lang với mắm gừng thơm thơm đậm đậm ngon `
+                    ],
+                },
+                {
+                    name: `Canh mướp nấu rau dền`,
+                    ingredient: [`2 quả mướp hương nhỏ`, `1 bó rau dền`, `1 thìa canh nước mắm`, `1 thìa cà phê nhỏ bột nêm nấm`],
+                    making: [
+                        `Bước 1: Thái nhỏ rau dền`,
+                        `Bước 2: Phi thơm hành tỏi xong cho mướp vào xào, lửa to đảo nhanh, nêm gia vị vào, đỏ nước sôi vào`,
+                        `Bước 3: Đồng thời cho rau dền vào, đun sôi trở lại thì nhắc ngay nồi ra`
+                    ],
+                }],
+                price: `50.000 vnd`,
+                price_number: 50000, 
+                numberPeople: `4`
+            }]
+        }]
+    },
+    {
+        id: `2`,
+        titleChild: `Ẩm thực nước ngoài`,
+        listDish: [{
+            id: `1`,
+            name: `Stove Pot Roast With Mashed Potatoes`,
+            describe:`682 calo; chất đạm 31,2g; cacbohydrat 62,6g; chất béo 34,6g; cholesterol 120,3mg; natri 756,7mg`,
+            ingredient: [`(3 pound) thịt bò nướng`, `muối và tiêu đen xay để nếm`, `4 lon nước dùng bò cô đặc (chẳng hạn như Campbell's®)`, `1 ly nước`, `1 củ hành trắng cắt miếng lớn`, `5 tép tỏi`, `1 gói (16 ounce) cà rốt, gọt vỏ`, `1 nhánh hương thảo tươi`, `Khoai tây nghiền:5 pound khoai tây Yukon Gold, gọt vỏ và cắt hạt 1 (12 ounce) có thể là sữa bay hơi, hoặc khi cần thiết`, `½ cốc bơ`, `muối để nếm`],
+            making: [
+                `Bước 1:Nêm món thịt nướng với muối và tiêu đen; nướng trong chảo lớn, sâu lòng hoặc lò nướng kiểu Hà Lan ở lửa vừa cho đến khi chín vàng, khoảng 10 phút mỗi mặt`,
+                `Bước 2: Đổ nước luộc thịt bò và nước vào chảo rang. Xếp hành và tỏi băm xung quanh miếng thịt. Trải cà rốt rang trên đỉnh và đặt nhánh hương thảo trên đỉnh cà rốt. Vặn lửa ở mức vừa phải và đun nhỏ lửa cho đến khi chín mềm, khoảng 6 giờ`,
+                `Bước 3:Cho khoai tây vào nồi lớn đun sôi; giảm lửa nhỏ và đun cho đến khi mềm, khoảng 30 phút. Xả nước. Nghiền khoai tây với bơ và một nửa lượng sữa đã bốc hơi cho đến khi mịn; từ từ nghiền sữa bay hơi còn lại vào khoai tây để đạt được độ đặc mong muốn. Mùa muối.`,
+                `Bước 4:Lấy 1 hoặc 2 tép tỏi ra khỏi chảo và băm nhuyễn các tép tỏi trên lớp rang; phục vụ với khoai tây nghiền.`
+            ],
+            price: `100.000 vnd`,
+            price_number: 100000,
+            numberPeople: `4`,
+            prepare: `30 minutes`,
+            cook: `6 hours`
+        }]
+    }]
+},
+{
+    title: `Market`,
+    id: `2`,
+    food: [{
+        titleChild: `Meat`,
+        id: `1`,
+        listFood: [{
+            id: `1`,
+            name: `Thịt heo Mỹ`,
+            quantitative: `500g`,
+            price: `100.000 vnd`,
+            price_number: 100000
+        },
+        {
+            id: `2`,
+            name: `Thịt bò Mỹ`,
+            quantitative: `500g`,
+            price: `45.000 vnd`,
+            price_number: 45000
+        }]
+    },
+    {
+        titleChild: `Drinking`,
+        id: `2`,
+        listFood: [{
+            id: `1`,
+            name: `Coca`,
+            quantitative: 1,
+            price: `10.000 vnd`,
+            price_number: 10000,
+        },
+        {
+            id: `2`,
+            name: `7-Up`,
+            quantitative: 1,
+            price: `15.000 vnd`,
+            price_number: 15000,
+        }]
+    }]
+}]
